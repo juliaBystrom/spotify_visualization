963f05449495bb419541f2b6aeb9b615a3ef4c22
Julia Byström <juliavasastan@gmail.com>
2021-05-04 18:06:36 +0200

    Add error handeling for when a user denies accses to spotify

--- server/app.js
+++ server/app.js
@@ -23,32 +23,40 @@ app.get("/spotify_login", function (req, res) {
 
 app.get("/callback", function (req, res) {
   let code = req.query.code || null;
+  let error = req.query.error || null;
 
-  
+  // If an error is returned let the client know
+  if (error) {
+    console.log(`[Server] Error occured from spotify authorize:  ${error}`);
 
-  let authOptions = {
-    url: "https://accounts.spotify.com/api/token",
-    form: {
-      code: code,
-      redirect_uri: redirect_uri,
-      grant_type: "authorization_code",
-    },
-    headers: {
-      Authorization:
-        "Basic " +
-        new Buffer.from(
-          process.env.CLIENT_ID +
-            ":" +
-            process.env.CLIENT_SECRET
-        ).toString("base64"),
-    },
-    json: true,
-  };
-  request.post(authOptions, function (error, response, body) {
-    var access_token = body.access_token;
+    let errorMessage =
+      error === "access_denied" ? "access_denied" : "serverError";
     let uri = process.env.FRONTEND_URI || "http://localhost:3000";
-    res.redirect(uri + "?access_token=" + access_token);
-  });
+    res.redirect(uri + "?error=" + errorMessage);
+
+  } else {
+    let authOptions = {
+      url: "https://accounts.spotify.com/api/token",
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: "authorization_code",
+      },
+      headers: {
+        Authorization:
+          "Basic " +
+          new Buffer.from(
+            process.env.CLIENT_ID + ":" + process.env.CLIENT_SECRET
+          ).toString("base64"),
+      },
+      json: true,
+    };
+    request.post(authOptions, function (error, response, body) {
+      var access_token = body.access_token;
+      let uri = process.env.FRONTEND_URI || "http://localhost:3000";
+      res.redirect(uri + "?access_token=" + access_token);
+    });
+  }
 });
 
 let port = process.env.PORT || 8888;
